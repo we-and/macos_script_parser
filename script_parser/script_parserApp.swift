50ed25b8fa50d5efcfe141df616f61df654a73e6
JD <jbmdumont@gmail.com>
2024-05-25 07:13:34 +0100

    Initial Commit

---
+++ script_parser/script_parserApp.swift
@@ -0,0 +1,17 @@
+//
+//  script_parserApp.swift
+//  script_parser
+//
+//  Created by Jean Dumont on 25/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct script_parserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
